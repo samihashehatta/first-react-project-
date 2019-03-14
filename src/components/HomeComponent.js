a4b5408e8908aa969ada1d705a2dead5ee7a3d6d
samihashehatta <samihashehatta@gmail.com>
2019-03-14 06:22:18 +0200

    adding react router

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+ 
+
+
+function Home (props){
+    return (
+        <div className="container">
+            <h4>Home</h4>
+            <p>
+                as;d;asd;sl;d;las;dl;sald;sld;sld;sld;ls;dls;ld;sld;lsldas
+                defaultasld
+                'asld's
+                asldsald'lsldasld'
+                aslddlasl'
+                defaultasldlsdl'slasld'
+                lsldassald
+                ls
+                'dldlasld
+                'sldl's
+                dldlasldld
+                'sldsald'
+                asldsaldd
+
+            </p>
+        </div>
+    );
+}
+export default Home;
